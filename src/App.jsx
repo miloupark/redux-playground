a5be327603df9fda4298ae2b8772f92298bbb6fb
hyebinpark <hoonywhitesocks@gmail.com>
2025-08-26 13:17:42 +0900

    feat(reducer): App에 counter reducer 임시 구현

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,19 @@ import "./App.css";
 // };
 // → action.js로 분리하고, 상수/함수로 관리
 
+// 액션 type에 따라 상태를 변경하는 함수
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+// → reducer.js로 분리 예정
+
 function App() {
   return (
     <>
